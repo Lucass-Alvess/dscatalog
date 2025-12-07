1832ccc8c42fc9c9f9cd9705d86a4e01e5a5e8b5
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2025-12-07 17:40:59 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dscatalog.entities;
 
-import java.util.Objects;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "tb_category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
