fc955abc59ee08bb85f20a1aba82244c4859c444
Lucass_Alvess <lucas_oliveira-01@outlook.com>
2026-04-02 18:17:08 -0300

    WhenIdExists

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -14,5 +18,30 @@ public class ProductServiceTests {
 
     @Mock
     private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+
+        Mockito.when(repository.existsById(existingId)).thenReturn(true);
+        Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
+
+
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists() {
+
+        Assertions.assertDoesNotThrow(() -> {
+            service.delete(existingId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+
+    }
 
 }
